06ecf5182d876a81e172b25d14fcdfba59e1c1d4
Francisco Perez Ramos <franperez21896@gmail.com>
2018-10-28 21:37:20 -0300

    Ya se puede persistir un pedido

--- src/main/java/backend/entities/Pedido.java
+++ src/main/java/backend/entities/Pedido.java
@@ -2,22 +2,38 @@ package backend.entities;
 
 import backend.entities.chocolates.Chocolate;
 
+import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
+@Entity
 public class Pedido {
 
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    @Column(name = "Pedido_id")
+    private int id;
+
+    @OneToMany(cascade = {CascadeType.ALL})
     private List<Chocolate> chocolates;
 
     public Pedido(){
-        this.chocolates = new ArrayList<Chocolate>();
+        this.chocolates = new ArrayList<>();
     }
 
     public void agregarChocolate(Chocolate choco1) {
         this.chocolates.add(choco1);
     }
 
+    public List<Chocolate> getChocolates(){
+        return this.chocolates;
+    }
+
     public double getPrecio() {
         return chocolates.stream().mapToDouble(Chocolate::precio).sum();
+    }
+
+    public Integer getId() {
+        return id;
     }
 }
