b3ef7a7334c4d9f52c33d3511f16dc89d1b11a2e
mariabelenamat <belen.amat29@gmail.com>
2018-10-31 19:34:04 -0300

    agrego cliente y sus test y consulta para saber los pedidos de un cliente

--- backend/src/main/java/backend/entities/Pedido.java
+++ backend/src/main/java/backend/entities/Pedido.java
@@ -1,10 +1,13 @@
 package backend.entities;
 
+import backend.entities.Cliente;
 import backend.entities.chocolates.Chocolate;
 
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
+
+import static org.junit.Assert.assertEquals;
 
 @Entity
 public class Pedido {
@@ -17,8 +20,12 @@ public class Pedido {
     @OneToMany(cascade = {CascadeType.ALL})
     private List<Chocolate> chocolates;
 
-    public Pedido(){
+    @Column
+    private Integer dniClientePedido;
+
+    public Pedido(Cliente cliente){
         this.chocolates = new ArrayList<>();
+        this.dniClientePedido = cliente.getDni();
     }
 
     public void agregarChocolate(Chocolate choco1) {
@@ -36,4 +43,17 @@ public class Pedido {
     public Integer getId() {
         return id;
     }
+
+    public void quitarChocolate(Chocolate c) {
+        chocolates.remove(c);
+    }
+
+    public Integer getCantidadChocolates() {
+        return this.getChocolates().size();
+    }
+
+    public Integer getDNICliente(){
+        return this.dniClientePedido;
+    }
 }
+
