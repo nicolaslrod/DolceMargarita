6b9ed72691271d54e8299854ca383a59b08b6b3e
leavalos <l.e.avalos1@gmail.com>
2018-11-04 04:18:04 -0300

    Se comienza a crear ApiRestFull

---
+++ backend/src/main/java/server/App.java
@@ -0,0 +1,12 @@
+package server;
+
+import org.eclipse.jetty.server.Handler;
+import org.uqbar.xtrest.api.XTRest;
+
+@SuppressWarnings("all")
+public class App {
+    public static void main(final String[] args) {
+        Server _restfulServer = new Server();
+        XTRest.startInstance(9003,  _restfulServer);
+    }
+}
